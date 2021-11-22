d53d9505c4bc982cd5e283139eee9c0b33c7934e
Takashiro Kazama <kazamanbou1209.pfm@gmail.com>
2021-11-23 02:55:57 +0900

    Refactoring

--- src/components/styles/Slider.ts
+++ src/components/styles/Slider.ts
@@ -7,46 +7,51 @@ interface StyledSliderWrapperProps {
 }
 
 export const StyledSliderWrapper = styled.div<StyledSliderWrapperProps>`
-  overflow: hidden;
-  position: relative;
-  background: #333132;
-  padding: ${(props) => (props.zoomFactor / props.visibleSlides) * 0.7 + "%"} 0;
+  ${({ zoomFactor, visibleSlides }) => `
+    overflow: hidden;
+    position: relative;
+    background: #333132;
+    padding: ${(zoomFactor / visibleSlides) * 0.7 + "%"} 0;
+  `}
 `;
 
 interface StyledSliderProps {
   visibleSlides: number;
+  pageTransition: number;
   transformValue: string;
   zoomFactor: number;
   slideMargin: number;
-  pageTransition: number;
   ref: any;
 }
 
 export const StyledSlider = styled.div<StyledSliderProps>`
-  display: flex;
-  padding: 0 55px;
-  transition: transform ${(props) => props.pageTransition}ms ease;
+  ${({ pageTransition, transformValue }) => `
+    display: flex;
+    padding: 0 55px;
+    transition: transform ${pageTransition}ms ease;
 
-  :hover ${StyledSliderItem} {
-    transform: translateX(${(props) => props.transformValue});
-  }
+    :hover ${StyledSliderItem} {
+      transform: translateX(${transformValue});
+    }
+  `}
 `;
 
 interface StyledButtonWrapperProps {
-  zoomFactor: number;
   isForward: boolean;
+  zoomFactor: number;
 }
 
 export const StyledButtonWrapper = styled.div<StyledButtonWrapperProps>`
-  position: absolute;
-  border-radius: ${(props) =>
-    props.isForward ? "0.5vw 0 0 0.5vw" : "0 0.5vw 0.5vw 0"};
-  box-sizing: border-box;
-  top: 0;
-  ${(props) => (props.isForward ? "right: 0;" : "left: 0;")};
-  width: 55px;
-  height: 100%;
-  padding: ${(props) => props.zoomFactor / 8 + "%"} 0;
+  ${({ isForward, zoomFactor }) => `
+    position: absolute;
+    border-radius: ${isForward ? "0.5vw 0 0 0.5vw" : "0 0.5vw 0.5vw 0"};
+    box-sizing: border-box;
+    top: 0;
+    ${isForward ? "right: 0;" : "left: 0;"};
+    width: 55px;
+    height: 100%;
+    padding: ${zoomFactor / 8 + "%"} 0;
+  `}
 `;
 
 interface StyledButtonProps {
@@ -54,24 +59,25 @@ interface StyledButtonProps {
 }
 
 export const StyledButton = styled.button<StyledButtonProps>`
-  display: block;
-  background: rgb(0, 0, 0, 0.7);
-  border: 0;
-  border-radius: ${(props) =>
-    props.isForward ? "0.5vw 0 0 0.5vw" : "0 0.5vw 0.5vw 0"};
-  top: 0;
-  ${(props) => (props.isForward ? "right: 0;" : "left: 0;")};
-  width: 100%;
-  height: 100%;
-  color: #fff;
-  font-size: 3rem;
-  font-weight: 800;
-  cursor: pointer;
-  outline: none;
-  transition: all 0.7s;
-  user-select: none;
+  ${({ isForward }) => `
+    display: block;
+    background: rgb(0, 0, 0, 0.7);
+    border: 0;
+    border-radius: ${isForward ? "0.5vw 0 0 0.5vw" : "0 0.5vw 0.5vw 0"};
+    top: 0;
+    ${isForward ? "right: 0;" : "left: 0;"};
+    width: 100%;
+    height: 100%;
+    color: #fff;
+    font-size: 3rem;
+    font-weight: 800;
+    cursor: pointer;
+    outline: none;
+    transition: all 0.7s;
+    user-select: none;
 
-  :hover {
-    opacity: 0.5;
-  }
+    :hover {
+      opacity: 0.5;
+    }
+  `}
 `;
--- src/components/styles/SliderItem.ts
+++ src/components/styles/SliderItem.ts
@@ -8,49 +8,47 @@ interface StyledSliderItemProps {
 }
 
 export const StyledSliderItem = styled.div<StyledSliderItemProps>`
-  margin: 0 ${(props) => props.slideMargin}px;
-  transition: transform 500ms ease;
-  border-radius: 0.5vw;
-  cursor: pointer;
-  width: 100%;
-  height: 100%;
-  box-sizing: border-box;
-  display: flex;
-  transform: scale(1);
-  user-select: none;
-  flex: 0 0
-    calc(
-      100% / ${(props) => props.visibleSlides} -
-        ${(props) => props.slideMargin * 2}px
-    );
-
-  img {
-    height: 100%;
-    width: 100%;
+  ${({ slideMargin, visibleSlides, zoomFactor }) => `
+    margin: 0 ${slideMargin}px;
+    transition: transform 500ms ease;
     border-radius: 0.5vw;
+    cursor: pointer;
+    width: 100%;
+    height: 100%;
     box-sizing: border-box;
-    -webkit-user-drag: none;
-  }
+    display: flex;
+    transform: scale(1);
+    user-select: none;
+    flex: 0 0 calc(100% / ${visibleSlides} - ${slideMargin * 2}px);
 
-  :hover {
-    transform: scale(${(props) => props.zoomFactor / 100 + 1}) !important;
-  }
-
-  :hover ~ * {
-    transform: translateX(${(props) => props.zoomFactor / 2 + "%"}) !important;
-  }
-
-  &.left {
-    transform-origin: left;
-    :hover ~ * {
-      transform: translateX(${(props) => props.zoomFactor + "%"}) !important;
+    img {
+      height: 100%;
+      width: 100%;
+      border-radius: 0.5vw;
+      box-sizing: border-box;
+      -webkit-user-drag: none;
     }
-  }
 
-  &.right {
-    transform-origin: right;
-    :hover ~ * {
-      transform: translateX(0%) !important;
+    :hover {
+      transform: scale(${zoomFactor / 100 + 1}) !important;
     }
-  }
+
+    :hover ~ * {
+      transform: translateX(${zoomFactor / 2 + "%"}) !important;
+    }
+
+    &.left {
+      transform-origin: left;
+      :hover ~ * {
+        transform: translateX(${zoomFactor + "%"}) !important;
+      }
+    }
+
+    &.right {
+      transform-origin: right;
+      :hover ~ * {
+        transform: translateX(0%) !important;
+      }
+    }
+  `}
 `;
